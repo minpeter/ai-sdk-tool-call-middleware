512db59393c9c4275762106e950ea3bebfe0ec07
minpeter <minpeter@friendli.ai>
2025-04-10 16:12:40 +0900

    Refactor transformParams to move originalToolDefinitions declaration after processedPrompt mapping

--- src/hermes-middleware.ts
+++ src/hermes-middleware.ts
@@ -38,12 +38,6 @@ export const hermesToolMiddleware: LanguageModelV1Middleware = {
 
   // @ts-ignore
   transformParams: async ({ params }) => {
-    // Appropriate fixes are needed as they are disappearing in LanguageModelV2
-    const originalToolDefinitions =
-      params.mode.type === "regular" && params.mode.tools
-        ? params.mode.tools
-        : {};
-
     const processedPrompt = params.prompt.map((message) => {
       if (message.role === "assistant") {
         return {
@@ -62,9 +56,7 @@ export const hermesToolMiddleware: LanguageModelV1Middleware = {
             return content;
           }),
         };
-      }
-
-      if (message.role === "tool") {
+      } else if (message.role === "tool") {
         return {
           role: "user",
           content: [
@@ -87,13 +79,19 @@ export const hermesToolMiddleware: LanguageModelV1Middleware = {
       return message;
     });
 
+    // Appropriate fixes are needed as they are disappearing in LanguageModelV2
+    const originalToolDefinitions =
+      params.mode.type === "regular" && params.mode.tools
+        ? params.mode.tools
+        : {};
+
     const HermesPromptFormat = (tools: string) =>
       `You are a function calling AI model. You are provided with function signatures within <tools></tools> XML tags. You may call one or more functions to assist with the user query. Don't make assumptions about what values to plug into functions. Here are the available tools: <tools>` +
       tools +
       `</tools> Use the following pydantic model json schema for each tool call you will make: {'title': 'FunctionCall', 'type': 'object', 'properties': {'arguments': {'title': 'Arguments', 'type': 'object'}, 'name': {'title': 'Name', 'type': 'string'}}, 'required': ['arguments', 'name']} For each function call return a json object with function name and arguments within <tool_call></tool_call> XML tags as follows:
-<tool_call>
-{'arguments': <args-dict>, 'name': <function-name>}
-</tool_call>`;
+    <tool_call>
+    {'arguments': <args-dict>, 'name': <function-name>}
+    </tool_call>`;
 
     const HermesPrompt = HermesPromptFormat(
       JSON.stringify(Object.entries(originalToolDefinitions))
