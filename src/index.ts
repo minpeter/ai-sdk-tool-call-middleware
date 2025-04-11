dea3b0a0cc8e6d6afef630ac2a944d4a93b750d6
minpeter <kali2005611@gmail.com>
2025-04-12 00:23:39 +0900

    Update hermesToolMiddleware to use Markdown-style tags for tool calls and responses; modify prompt for improved clarity

--- src/index.ts
+++ src/index.ts
@@ -16,16 +16,16 @@ async function main() {
       model: openrouter("google/gemma-3-27b-it"),
       // model: openrouter("nousresearch/hermes-3-llama-3.1-70b"),
       middleware: hermesToolMiddleware({
-        toolCallTag: "<tool_call>",
-        toolCallEndTag: "</tool_call>",
-        toolResponseTag: "<tool_response>",
-        toolResponseEndTag: "</tool_response>",
+        toolCallTag: "```tool_call\n",
+        toolCallEndTag: "\n```",
+        toolResponseTag: "```tool_response\n",
+        toolResponseEndTag: "\n```",
         toolSystemPromptTemplate(tools) {
           return `You have access to functions. If you decide to invoke any of the function(s),
 you MUST put it in the format of
-<tool_call>
+\`\`\`tool_call
 {'name': <function-name>, 'arguments': <args-dict>}
-</tool_call>
+\`\`\`
 
 You SHOULD NOT include any other text in the response if you call a function
 
@@ -34,9 +34,21 @@ ${tools}`;
       }),
     }),
     system: "You are a helpful assistant.",
-    prompt: "What is the weather in New York and Los Angeles?",
+    // prompt: "What is the weather in New York and Los Angeles?",
+    prompt: "What is the weather in my city?",
     maxSteps: 4,
     tools: {
+      get_location: {
+        description: "Get the User's location.",
+        parameters: z.object({}),
+        execute: async () => {
+          // Simulate a location API call
+          return {
+            city: "New York",
+            country: "USA",
+          };
+        },
+      },
       get_weather: {
         description:
           "Get the weather for a given city. " +
@@ -58,8 +70,12 @@ ${tools}`;
   for await (const part of result.fullStream) {
     if (part.type === "text-delta") {
       process.stdout.write(part.textDelta);
-    } else {
-      // console.log(part);
+    } else if (part.type === "tool-result") {
+      console.log({
+        name: part.toolName,
+        args: part.args,
+        result: part.result,
+      });
     }
   }
 
