fc32eecd37c860ab7ddd4f589806d2ef3aac8cb7
wanghsinche <wanghsinche@hotmail.com>
2025-08-15 04:57:34 +0000

    fix the words

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -3,7 +3,7 @@ import { createToolMiddleware } from "./tool-call-middleware";
 const gemmaToolMiddleware = createToolMiddleware({
   toolSystemPromptTemplate(tools) {
     return `You have access to functions. If you decide to invoke any of the function(s),
-you MUST put it in the format of markdown code fense block with the language name of tool_call , e.g.
+you MUST put it in the format of markdown code fence block with the language name of tool_call , e.g.
 \`\`\`tool_call
 {'name': <function-name>, 'arguments': <args-dict>}
 \`\`\`
@@ -11,7 +11,7 @@ You SHOULD NOT include any other text in the response if you call a function
 ${tools}`;
   },
   toolCallTag: "```tool_call\n",
-  toolCallEndTag: "\n``",  // two quote are more common in gemma output
+  toolCallEndTag: "\n``",  // two backticks are more common in gemma output
   toolResponseTag: "```tool_response\n",
   toolResponseEndTag: "\n```",
 });
