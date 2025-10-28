6c1f0d92b102aa72bbf4f215a8b39b82f43b2b60
Woonggi Min <kali2005611@gmail.com>
2025-10-28 02:10:47 +0000

    fmt mig by cursor-agent

---
+++ test-biome.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /data/minpeter/github.com/minpeter/ai-sdk-tool-call-middleware
+./node_modules/.bin/biome check packages/parser/src/utils/robust-json.ts packages/rxml/src/core/parser.ts
